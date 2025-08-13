f8e33e02e183122b87aa7397f3e83a4aa43112a4
brotmensch <brotmensch@gmail.com>
2025-08-13 14:10:25 +0200

    changed harcoded book path to arg

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from stats import count_words
 from stats import count_characters
 from stats import sort_char_dict
+import sys
 def main():
+
+    
     #num=count_characters("books/frankenstein.txt")
     #'print(count_words("books/frankenstein.txt"))
     #print (num)
     #print(sort_char_dict("books/frankenstein.txt"))
-    word_count=count_words("books/frankenstein.txt")
-    char_list=sort_char_dict("books/frankenstein.txt")
+   
+
+    if len(sys.argv)!=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path_to_book=(sys.argv[1])
+    word_count=count_words(path_to_book)
+    char_list=sort_char_dict(path_to_book)
+
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_book}...")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
