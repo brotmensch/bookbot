0e48eb31a5417feb2747388dc1e62e3514b4057c
brotmensch <brotmensch@gmail.com>
2025-08-13 10:42:37 +0200

    refactored wordcount to stats

--- stats.py
+++ stats.py
@@ -2,3 +2,9 @@ def count_words(t):
     text=get_book_text(t)
     words=text.split()
     return len(words)
+
+def get_book_text(b):
+    with open(b) as f:
+        book_content=f.read()
+
+    return book_content
